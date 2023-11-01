982816ee8a8f55d1a07f1eae3e70bfa0904b1d2b
Vasilii <VVasiGoydin@sberbank.ru>
2023-11-01 20:08:34 +0300

    1 задание на условия

---
+++ 02 - functions/lesson/index.js
@@ -0,0 +1,15 @@
+// 1 задание
+const name = "Vasilii";
+const name2 = "Nikita";
+
+if (name === "Vasilii") {
+  console.log(`Приветствую, ${name}`);
+} else {
+  console.log("Вы еще не зарегистрированы. Зарегистроваться?");
+}
+
+if (name2 === "Vasilii") {
+  console.log(`Приветствую, ${name}`);
+} else {
+  console.log("Вы еще не зарегистрированы. Зарегистроваться?");
+}
