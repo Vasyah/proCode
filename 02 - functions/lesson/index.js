9767de9c43d0dba421adaeaf5c833dff03270f7f
Vasilii <VVasiGoydin@sberbank.ru>
2023-11-01 20:37:14 +0300

    4 задание - функции

--- 02 - functions/lesson/index.js
+++ 02 - functions/lesson/index.js
@@ -44,5 +44,35 @@ function checkUser(userName) {
   }
 }
 
-checkUser('Виолетта');
-checkUser('Vasilii');
+checkUser("Виолетта");
+checkUser("Vasilii");
+
+//4 задание
+/**
+ * Функция проверяет здоровье персонажа и выводит его на экран
+ * @param {*} health - текущее здоровье персонажа
+ */
+const checkHealth = (health) => {
+  if (health === 100) {
+    console.log("Вы здоровы");
+  } else if (health < 100 && health >= 75) {
+    // от 75-99 жизней
+    console.log("Не помешало бы подлечиться");
+  } else if (health < 75 && health >= 50) {
+    // от 50-74 жизней
+    console.log("Желательно подлечиться");
+  } else if (health < 50 && health > 0) {
+    // от 1-49 жизней
+    console.log("Срочно надо подлечиться, уровень здоровья слишком низкий");
+  } else if (health === 0) {
+    // 0 жизней
+    console.log("Вы мертвы");
+  } else {
+    console.log(`Ваш уровень здорвья: ${health}`);
+  }
+};
+
+checkHealth(100);
+checkHealth(75);
+checkHealth(50);
+checkHealth(0);
