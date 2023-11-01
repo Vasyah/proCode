7506edd6432e377b632763001fde35a308e1b42d
Vasilii <VVasiGoydin@sberbank.ru>
2023-11-01 20:22:04 +0300

    2 задание

--- 02 - functions/lesson/index.js
+++ 02 - functions/lesson/index.js
@@ -13,3 +13,24 @@ if (name2 === "Vasilii") {
 } else {
   console.log("Вы еще не зарегистрированы. Зарегистроваться?");
 }
+
+// 2 задание
+const health = 75;
+
+if (health === 100) {
+  console.log("Вы здоровы");
+} else if (health < 100 && health >= 75) {
+  // от 75-99 жизней
+  console.log("Не помешало бы подлечиться");
+} else if (health < 75 && health >= 50) {
+  // от 50-74 жизней
+  console.log("Желательно подлечиться");
+} else if (health < 50 && health > 0) {
+  // от 1-49 жизней
+  console.log("Срочно надо подлечиться, уровень здоровья слишком низкий");
+} else if (health === 0) {
+  // 0 жизней
+  console.log("Вы мертвы");
+} else {
+  console.log(`Ваш уровень здорвья: ${health}`);
+}
