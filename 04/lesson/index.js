ce76308ef8abe3a14bab4795e32c80db862a741b
Vasilii <VVasiGoydin@sberbank.ru>
2023-11-09 13:08:18 +0300

    рефакторинг

--- 04/lesson/index.js
+++ 04/lesson/index.js
@@ -1,37 +1,65 @@
-
 const taskData = {
-    id: 700,
-    isEnable: true,
-    title: 'Заголовок задачи',
-    subTitle: 'Подзаголовок'
-}
+  id: 700,
+  isEnable: true,
+  title: "Заголовок задачи",
+  subTitle: "Подзаголовок",
+};
 
 const tasksContainter = document.querySelector(".tasks");
 
-const itemContainer = document.createElement("div");
-const itemCheckbox = document.createElement("input");
+/**
+ * Функция создаёт элемент Checkbox
+ * @param {Ф} id
+ * @param {*} isEnable
+ * @returns
+ */
+const createCheckbox = (id, isEnable) => {
+  const checkbox = document.createElement("input");
 
-// блок с информацией о задачах
-const itemDescription = document.createElement("div");
-const itemTitle = document.createElement("p");
-const itemSubtitle = document.createElement("p");
+  checkbox.type = "checkbox";
+  checkbox.id = `${id}`;
+  checkbox.name = `${id}`;
+  checkbox.checked = isEnable;
+  checkbox.classList = "item-checkbox";
 
-itemCheckbox.type = "checkbox";
-itemCheckbox.id = `${taskData.id}`;
-itemCheckbox.name = `${taskData.id}`;
-itemCheckbox.checked = taskData.isEnable;
-itemCheckbox.classList = "item-checkbox";
+  return checkbox;
+};
 
-itemDescription.classList = "item-description";
+/**
+ * Функция создаёт элемент с описанием задачи
+ * @param {*} title - заголовок
+ * @param {*} subTitle - подзаголовок
+ * @returns
+ */
+const createDescription = (title, subTitle) => {
+  // блок с информацией о задачах
+  const itemDescription = document.createElement("div");
+  const itemTitle = document.createElement("p");
+  const itemSubtitle = document.createElement("p");
 
-itemTitle.textContent = `${taskData.title}`;
-itemTitle.classList = "item-title";
+  itemDescription.classList = "item-description";
 
-itemSubtitle.textContent = `${taskData.subTitle}`;
-itemSubtitle.classList = "item-subtitle";
-itemContainer.classList = "item";
+  itemTitle.textContent = `${title}`;
+  itemTitle.classList = "item-title";
 
-itemDescription.append(itemTitle, itemSubtitle);
-itemContainer.append(itemCheckbox, itemDescription);
+  itemSubtitle.textContent = `${subTitle}`;
+  itemSubtitle.classList = "item-subtitle";
 
-tasksContainter.append(itemContainer);
+  itemDescription.append(itemTitle, itemSubtitle);
+
+  return itemDescription;
+};
+
+const createItem = (task) => {
+  const itemContainer = document.createElement("div");
+  itemContainer.classList = "item";
+
+  itemContainer.append(
+    createCheckbox(task.id, task.isEnable),
+    createDescription(task.title, task.subTitle)
+  );
+
+  return itemContainer;
+};
+
+tasksContainter.append(createItem(taskData));
