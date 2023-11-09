4693d61df41e9e278d32370b80925d8b2b1613b2
Vasilii <VVasiGoydin@sberbank.ru>
2023-11-09 12:17:06 +0300

    1 итерация

--- 04/lesson/index.js
+++ 04/lesson/index.js
@@ -1,3 +1,27 @@
-//Новый файл
+const tasksContainter = document.querySelector(".tasks");
 
-//1 задание
+const itemContainer = document.createElement("div");
+const itemCheckbox = document.createElement("input");
+
+// блок с информацией о задачах
+const itemDescription = document.createElement("div");
+const itemTitle = document.createElement("p");
+const itemSubtitle = document.createElement("p");
+
+itemCheckbox.type = "checkbox";
+itemCheckbox.id = "checkbox";
+itemCheckbox.classList = "item-checkbox";
+
+itemDescription.classList = "item-description";
+
+itemTitle.textContent = "Заголовок";
+itemTitle.classList = "item-title";
+
+itemSubtitle.textContent = "Подзаголовок";
+itemSubtitle.classList = "item-subtitle";
+itemContainer.classList = "item";
+
+itemDescription.append(itemTitle, itemSubtitle);
+itemContainer.append(itemCheckbox, itemDescription);
+
+tasksContainter.append(itemContainer);
